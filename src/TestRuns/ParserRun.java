948caecd287a8ee8ac34a09c5be57ebc4327eac3
gaurav gupta <gaurav22gupta@gmail.com>
2015-02-13 19:14:44 -0800

    obs

--- src/TestRuns/ParserRun.java
+++ src/TestRuns/ParserRun.java
@@ -3,7 +3,7 @@ import Frontend.*;
 
 public class ParserRun {
 		public static void main(String []args){
-			String filename = "testprogs/arithemetic.txt";
+			String filename = "testprogs/test001.txt";
 			Parser parse = new Parser(filename);
 			BasicBlock bb = parse.compute();
 			System.out.println("\n!!! Parsed successfully !!!");
