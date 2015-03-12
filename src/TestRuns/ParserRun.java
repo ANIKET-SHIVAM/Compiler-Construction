00507b4c6628782080d95c8d5e22cf04961d5910
ANIKET SHIVAM <aniketshivam@gmail.com>
2015-03-12 16:55:14 -0700

    nop

--- src/TestRuns/ParserRun.java
+++ src/TestRuns/ParserRun.java
@@ -10,7 +10,7 @@ public class ParserRun {
 			
 
 			//String filename = "testprogs/arithemetic.txt";
-			String filename = "testprogs/test001.txt";
+			String filename = "testprogs/test002.txt";
 
 			Parser parse = new Parser(filename);
 			BasicBlock bb = parse.compute();
